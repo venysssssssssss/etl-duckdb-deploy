6a7c3e03a13c920d0f23300a8a163165ed7d13b6
venysssssssssss <shablaur7@hotmail.com>
2024-03-19 21:17:12 -0300

    Fix file extension comparison in a.py

--- a.py
+++ a.py
@@ -4,6 +4,6 @@ arquivos_csv = []
 todos_os_arquivos = os.listdir('pasta_gdown')
 for arquivo in todos_os_arquivos:
     print(todos_os_arquivos)
-    if arquivo.endswith(".csv"):
+    if arquivo.endswith('.csv'):
         caminho_completo = os.path.join('pasta_gdown', arquivo)
         arquivos_csv.append(caminho_completo)
